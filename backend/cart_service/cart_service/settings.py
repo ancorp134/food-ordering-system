7ed86dd3eb085eea72a9ea8f6b431b95b4da1268
Ankit Tyagi <ancorp007@gmail.com>
2025-12-21 20:29:35 +0530

    setup redis as a database for cart service

--- backend/cart_service/cart_service/settings.py
+++ backend/cart_service/cart_service/settings.py
@@ -56,6 +56,18 @@ REST_FRAMEWORK = {
     ),
 }
 
+# redis setup
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1", # We use DB 1 for Cart
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 ROOT_URLCONF = 'cart_service.urls'
 
 TEMPLATES = [
