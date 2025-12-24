bcab7d890373a26fb2f55181fb5ec45e01636dbf
Ankit Tyagi <ancorp007@gmail.com>
2025-12-24 10:21:32 +0530

    add order items schema

--- backend/order_service/orders/models.py
+++ backend/order_service/orders/models.py
@@ -28,3 +28,16 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id}"
+
+
+
+class OrderItem(models.Model):
+
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    menu_item_id = models.UUIDField()
+    quantity = models.PositiveIntegerField()
+    item_price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"Item {self.menu_item_id}"
