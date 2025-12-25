5fccd8fbe09f94647bc0ff3f6f4dceff0d895bb4
Ankit Tyagi <ancorp007@gmail.com>
2025-12-25 11:00:21 +0530

    create a post api for order service

--- backend/order_service/orders/views.py
+++ backend/order_service/orders/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .serializer import OrderSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+
+class OrderCheckOut(APIView):
+
+    permission_classes =  [IsAuthenticated]
+
+    def post(self,request):
+        serializer = OrderSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
