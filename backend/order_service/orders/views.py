f071962e02b8646bba846dc505080a78b17daa73
Ankit Tyagi <ancorp007@gmail.com>
2025-12-27 11:44:29 +0530

    create get api to fetch users orders

--- backend/order_service/orders/views.py
+++ backend/order_service/orders/views.py
@@ -4,13 +4,27 @@ from rest_framework.response import Response
 from .serializer import OrderSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-
+from .models import Order
 # Create your views here.
 
 
 class OrderCheckOut(APIView):
 
     permission_classes = [IsAuthenticated]
+
+
+    def get(self,request):
+        cust_id = request.user.token.get("user_id")
+
+        try:
+            orders = Order.objects.filter(customer_id = cust_id)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = OrderSerializer(orders,many=True)
+        if serializer.is_valid():
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request):
         serializer = OrderSerializer(data=request.data, context={"request": request})
