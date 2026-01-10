14551f16691b4c7b1324d85b51d48f298ca1d052
Ankit Tyagi <ancorp007@gmail.com>
2026-01-10 11:32:18 +0530

    fix kafka error

--- backend/notification_service/consumer.py
+++ backend/notification_service/consumer.py
@@ -1,14 +1,22 @@
 import json
 from kafka import KafkaConsumer
 
+def safe_deserialize(v):
+    if v is None:
+        return None
+    try:
+        return json.loads(v.decode("utf-8"))
+    except json.JSONDecodeError:
+        print(f"⚠️ Skipping invalid JSON: {v}")
+        return None
 
 consumer = KafkaConsumer(
     "order-events",
     bootstrap_servers=["localhost:9092"],
-    group_id="notification-group",   
-    auto_offset_reset="earliest", 
+    group_id="notification-group",
+    auto_offset_reset="earliest",
     enable_auto_commit=True,
-    value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+    value_deserializer=safe_deserialize, # Use the safe function here
     key_deserializer=lambda k: k.decode("utf-8") if k else None,
 )
 
@@ -16,6 +24,12 @@ print("📢 Notification Service started. Waiting for events...")
 
 for message in consumer:
     event = message.value
+
+    if event is None:
+        continue
+
+    
+
     event_type = event.get("event_type")
     data = event.get("data")
 
